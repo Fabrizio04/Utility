20f31db625e2733290b3bcef581690c8b70dec2f
Fabrizio04 <amorellif94@gmail.com>
2026-08-19 17:44:19 +0200

    Aggiunto NET-Func-Delegate

---
+++ .NET/NET-Func-Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program {
+
+    private static Func<string, string> convert = s => s.ToUpper();
+	
+    private static void ConcatenateString(Func<string, string, string> myFunc){
+        Console.WriteLine(myFunc.Invoke("Fabrizio","Amorelli"));
+    }
+
+    private static void FormatString(Func<string, string, string> myFunc){
+        string aa = myFunc("fabrizio","amorelli");
+        Console.WriteLine(aa);
+    }
+    
+    private static string Concatena(string a, string b){
+        return $"{a}-{b}";
+    }
+    
+    public static void Main(){
+        // passo una lambda expression al delegate
+        FormatString((str, str2) => {
+            return str.ToUpper() +"-"+ str2.ToUpper();
+        });
+
+        // passo un metodo al delegate
+        ConcatenateString(Concatena);
+
+        // uso un attributo
+        string a = convert("fabrizio");
+        Console.WriteLine(a);
+    }
+}
---
+++ .NET/NET-Func-Delegate/README.md
@@ -0,0 +1,4 @@
+# NET-Func-Delegate
+## Piccolo script di esempio per l'utilizzo del delegate Func
+
+Documentazione: https://learn.microsoft.com/it-it/dotnet/api/system.func-2?view=net-6.0
